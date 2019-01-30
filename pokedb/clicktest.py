10e33b7db8880370e72ed67d5306c6d7a2aeae90
Chris J.M <chris@euchre.us>
2019-01-29 22:28:03 -0500

    Add test file for a bug in readline/click

---
+++ pokedb/clicktest.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+# -*- coding: UTF-8 -*-
+# vim: set fileencoding=UTF-8 :
+
+import click
+# import readline
+
+
+"""
+This is a test file to show how click and readline don't get along
+
+Uncomment the instruction to jump to home in main to cause a click error on
+prompts that aren't at the command line
+
+Uncomment the readline import and then hit enter at the click prompt to reveal
+a lack of newline bug """
+
+
+# @click.command()
+@click.option(
+    'test',
+    prompt='Is this a test'
+)
+def home(test):
+    click.echo(test)
+
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello %s!' % name)
+    # home(None)
+
+if __name__ == '__main__':
+    hello()
+
