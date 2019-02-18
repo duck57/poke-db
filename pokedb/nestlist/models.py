7560e0dc9c6beebbb83b561d4e4041bf1a5aad62
Chris J.M <chris@euchre.us>
2019-02-17 23:33:05 -0500

    First available HTML nest post

--- pokedb/nestlist/models.py
+++ pokedb/nestlist/models.py
@@ -127,6 +127,12 @@ class NstNeighborhood(models.Model):
     def __str__(self):
         return self.name
 
+    def place_name(self):
+        if self.region is None:
+            return self.name
+        else:
+            return self.region.name
+
 
 class NstParkSystem(models.Model):
     name = models.CharField(max_length=123)
--- pokedb/nestlist/views.py
+++ pokedb/nestlist/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+from .models import NstSpeciesListArchive, NstMetropolisMajor, NstLocation, NstRotationDate
+
+
+class CityView(generic.ListView):
+    model = NstSpeciesListArchive
+    template_name = 'nestlist/city.html'
+    context_object_name = 'current_nest_list'
+
+    def get_rotation(self):
+        return NstRotationDate.objects.filter(date__lte=timezone.now()).order_by('-num')[0]
+
+    def get_queryset(self):
+        return NstSpeciesListArchive.objects.filter(nestid__neighborhood__major_city=1,
+                                                    rotation_num=self.get_rotation())
+
+
+class IndexView(generic.ListView):
+    model = NstMetropolisMajor
+    template_name = 'nestlist/index.html'
+    context_object_name = 'city_list'
+
+    def get_queryset(self):
+        return NstMetropolisMajor.objects.all()
+
+
+class NestView(generic.DetailView):
+    model = NstLocation
+    template_name = 'nestlist/nest.html'
