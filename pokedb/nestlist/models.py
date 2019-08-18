2de507b463f7f89c33fdfa3d2b4019b11e827d90
Chris J.M <chris@euchre.us>
2019-08-17 23:43:39 -0400

    remove useless params

--- pokedb/nestlist/models.py
+++ pokedb/nestlist/models.py
@@ -105,7 +105,7 @@ class NstMetropolisMajor(models.Model):
     note = models.CharField(max_length=255, blank=True, null=True)
     admin_names = models.CharField(max_length=255, blank=True, null=True)
     airtable_base_id = models.CharField(max_length=30, blank=True, null=True)
-    airtable_bot_id = models.ForeignKey(NstAdminEmail, models.SET_NULL())
+    airtable_bot_id = models.ForeignKey(NstAdminEmail, models.SET_NULL)
 
     class Meta:
         managed = False
@@ -234,8 +234,8 @@ class NestSpecies(models.Model):
 
 
 class NstRawRpt(models.Model):
-    nsla_pk = models.ForeignKey(NstSpeciesListArchive, models.SET_NULL(), null=True)
-    bot_id = models.ForeignKey(NstAdminEmail, models.SET_NULL(), null=True)
+    nsla_pk = models.ForeignKey(NstSpeciesListArchive, models.SET_NULL, null=True)
+    bot_id = models.ForeignKey(NstAdminEmail, models.SET_NULL, null=True)
     user_name = models.CharField(max_length=120, blank=False, null=True)
     server_name = models.CharField(max_length=120, null=True, blank=True)
     nsla_pk_unlink = models.IntegerField(default=0)
@@ -243,8 +243,8 @@ class NstRawRpt(models.Model):
     foreign_db_row_num = models.IntegerField(null=True)
     raw_species_num = models.IntegerField(null=True)
     raw_species_txt = models.CharField(max_length=120, null=True)
-    attempted_dex_num = models.ForeignKey('speciesinfo.Pokemon', models.SET_NULL())
+    attempted_dex_num = models.ForeignKey('speciesinfo.Pokemon', models.SET_NULL)
     raw_park_info = models.CharField(max_length=120, null=True, blank=True)
-    parklink_id = models.ForeignKey(NstLocation, models.SET_NULL(), null=True)
+    parklink_id = models.ForeignKey(NstLocation, models.SET_NULL, null=True)
     action = models.IntegerField(null=True)
     dedupe_sig = models.CharField(null=True, blank=False, max_length=50)
