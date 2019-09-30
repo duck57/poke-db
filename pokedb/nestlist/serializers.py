27b8f01417f0447ab9c390c384b0ff272ea2e418
Chris J.M <chris@euchre.us>
2019-09-30 15:29:27 -0400

    Test serializer usage

--- pokedb/nestlist/serializers.py
+++ pokedb/nestlist/serializers.py
@@ -1,8 +1,44 @@
-from nestlist.models import NstLocation
+from nestlist.models import (
+    NstLocation,
+    NstMetropolisMajor,
+    NstNeighborhood,
+    NstCombinedRegion,
+)
 from rest_framework import serializers
 
 
-class ParkSerializer(serializers.HyperlinkedModelSerializer):
+class ParkSerializer(serializers.ModelSerializer):
+    neighborhood_name = serializers.ReadOnlyField(source="neighborhood.name")
+    neighborhood_id = serializers.ReadOnlyField(source="neighborhood")
+    # alt_names = serializers.ListSerializer(source="nstaltname_set")
+
     class Meta:
         model = NstLocation
-        fields = ["official_name", "short_name", "neighborhood"]
+        fields = [
+            "nestID",
+            "official_name",
+            "short_name",
+            "neighborhood_id",
+            "neighborhood_name",
+            "resident_history",
+            "pk",
+            "address",
+            "private",
+            "notes",
+            # "alt_names",
+        ]
+
+
+class CitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = NstMetropolisMajor
+
+
+class NeighborhoodSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = NstNeighborhood
+
+
+class RegionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = NstCombinedRegion
