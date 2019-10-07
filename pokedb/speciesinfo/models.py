22247e031e5fb90c5337af70b7e171033a8be38e
Chris J.M <chris@euchre.us>
2019-10-07 00:59:59 -0400

    master method for species searching

--- pokedb/speciesinfo/models.py
+++ pokedb/speciesinfo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models import Q
 from nestlist.utils import str_int
 
+
 # Create your models here.
 
 
@@ -173,6 +174,13 @@ class Pokemon(models.Model):
     nia_cust_hp = models.IntegerField(db_column="NIA_cust_HP", blank=True, null=True)
     nia_cust_atk = models.IntegerField(db_column="NIA_cust_ATK", blank=True, null=True)
     nia_cust_def = models.IntegerField(db_column="NIA_cust_DEF", blank=True, null=True)
+    previous_evolution = models.ForeignKey(
+        "Pokemon",
+        models.SET_NULL,
+        related_name="evolves_to",
+        null=True,
+        db_column="ef_fk_dj",
+    )
 
     class Meta:
         managed = False
@@ -217,33 +225,127 @@ class Pokemon(models.Model):
         return 1
 
 
-def match_species_by_name_or_number(sp_txt, only_one=False):
+def match_species_by_name_or_number(
+    sp_txt: str,
+    only_one: bool = False,
+    input_set=Pokemon.objects.all(),
+    age_up: bool = False,
+    previous_evolution_search: bool = False,
+):
     """
     :param sp_txt: pokémon name or number to search for
+                    if the "name" starts with "start" or is a type or region name, return those species
     :param only_one: set to True to enforce returning only a single pokémon
-    :return: a QuerySet of pokémon matching the input string if only_one is False
+    :param previous_evolution_search: set True to search for previous evolutions of matching species as well
+    :param age_up: set True to search for future evolutions of matching species
+    :param input_set: a QuerySet of Pokémon eligible to be results
+                    leave me null to search from all Pokémon
+    :return: a QuerySet of pokémon matching the input string if single_answer is False
     otherwise, return the pokémon that matches or raise an exception for multiple matches
-
-    Always returns None for empty lists
     """
 
-    # hardcoded Abra match so it does not match Crabwaler every time
-    if str(sp_txt).lower().strip() == "abra":
-        res_lst = [Pokemon.objects.get(pk="Abra")]
-    else:
-        res_lst = (
-            Pokemon.objects.filter(
-                Q(dex_number=sp_txt if str_int(sp_txt) else None)
-                | Q(name__icontains=sp_txt)
-            )
-            .order_by("dex_number")
-            .distinct()
+    def return_me(tentative_list, single_answer: bool):
+        """Inner function to handle allowing multiple results or not"""
+        tentative_list = tentative_list.order_by("dex_number").distinct()
+        if single_answer:
+            if not tentative_list:
+                return None
+            if len(tentative_list) == 1:
+                return tentative_list[0]
+            raise Pokemon.MultipleObjectsReturned
+        return tentative_list
+
+    def evo_searches(orig_pkmn, look_for_pre: bool, look_for_next: bool):
+        """
+        :param orig_pkmn: QuerySet assumed to contain a single pokémon
+        :param look_for_pre: same as previous_evolution_search from above
+        :param look_for_next: same as age_up from above
+        :return: sends the resulting QuerySet to return_me
+        """
+
+        def find_previous_pokemon(search_qset, doit):
+            if not doit:
+                return search_qset
+            return (
+                Pokemon.objects.filter(pk__in=search_qset.values("previous_evolution"))
+                .exclude(pk="(Egg)")
+                .distinct()
+                | search_qset.distinct()
+            ).distinct()
+
+        p = find_previous_pokemon(
+            find_previous_pokemon(orig_pkmn, look_for_pre), look_for_pre
         )
-    num_res = len(res_lst)
-    if num_res == 0:
+        future = (
+            input_set.filter(
+                Q(previous_evolution__in=orig_pkmn)
+                | Q(previous_evolution__previous_evolution__in=orig_pkmn)
+            )
+            if look_for_next
+            else input_set.none()
+        ).distinct()
+        return input_set.filter(pk__in=(p | future))
+
+    sp_txt = str(sp_txt).strip().lower()
+    if not sp_txt:
+        # return nothing if nothing is searched for
         return None
-    if only_one:
-        if num_res == 1:
-            return res_lst[0]
-        raise 9 from RuntimeError("Too many results")
-    return res_lst
+
+    # hardcoded Abra match so it does not match® Crabwaler every time
+    if sp_txt == "abra":
+        return return_me(Pokemon.objects.filter(pk="Abra"), only_one)
+
+    # return starters and their evolutions
+    if "start" in sp_txt:
+        return return_me(input_set.filter(category__in=[50, 52, 53]), only_one)
+
+    # handle numeric queries
+    if str_int(sp_txt):
+        me = Pokemon.objects.filter(dex_number=sp_txt)
+        # if you enter a number when looking for a single species, stop here
+        # stop if nothing was found, too
+        if only_one or not me:
+            return return_me(me, True)
+        return return_me(evo_searches(me, previous_evolution_search, age_up), False)
+
+    # handle short queries
+    if len(sp_txt) < 3:
+        return return_me(input_set.filter(name__icontains=sp_txt), only_one)
+
+    region_text_matches = input_set.filter(generation__region__icontains=sp_txt)
+    typed_matches = match_species_by_type(sp_txt, input_set)
+    # Queries matching a Region or Type do not get the previous/next evolution searches
+    if region_text_matches or typed_matches:
+        return return_me(region_text_matches | typed_matches, only_one)
+
+    # search by name and respect future/past evolution searching
+    return return_me(
+        input_set.filter(
+            pk__in=evo_searches(
+                Pokemon.objects.filter(name__icontains=sp_txt),
+                previous_evolution_search,
+                age_up,
+            )
+        ),
+        only_one,
+    )
+
+
+def nestable_species():
+    """Compare to the NestSpecies model in nestlist.models"""
+    return (
+        Pokemon.objects.filter(previous_evolution__category=6, form="Normal")
+        .exclude(category__pk__in=[6, 1, 2, 3, 41, 404, 77, 9999])
+        .order_by("dex_number")
+    )
+
+
+def match_species_by_type(target_type: str, input_list=Pokemon.objects.all()):
+    return input_list.filter(
+        Q(type1=target_type if str_int(target_type) else None)
+        | Q(
+            type2=target_type if str_int(target_type) else 6969420
+        )  # prevent matching on single-typed 'mon
+        | Q(type1__name__icontains=target_type)
+        | Q(type1__name__icontains=target_type)
+    ).order_by("dex_number")
