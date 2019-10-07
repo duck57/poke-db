5d9f032765b0d9d64f53e3a7e977ff407ba983c3
Chris J.M <chris@euchre.us>
2019-10-07 01:15:03 -0400

    move views.py to use the pokémon searching from speciesinfo
    deleted all the extra code from views.py as well

--- pokedb/nestlist/views.py
+++ pokedb/nestlist/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import (
     HttpResponseRedirect,
     HttpResponseBadRequest,
     Http404,
     HttpResponseNotFound,
 )
-from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView
-from .utils import parse_date, str_int
 from django.db.models import Q
 
 # Create your views here.
@@ -19,12 +16,11 @@ from .models import (
     NstSpeciesListArchive,
     NstMetropolisMajor,
     NstLocation,
-    NstRotationDate,
+    get_local_nsla_for_rotation,
     NstNeighborhood,
     get_rotation,
 )
-from speciesinfo.models import Pokemon, Generation
-from typeedit.models import Type
+from speciesinfo.models import match_species_by_name_or_number
 from .serializers import ParkSerializer
 
 
@@ -40,111 +36,6 @@ def report_nest(request, **kwargs):
     return HttpResponseRedirect("http://columbusnestreport.cf")
 
 
-def filter_nsla_by_species_name(out, sp_filter):
-    """
-
-    :param out:
-    :param sp_filter:
-    :return:
-    """
-
-    if len(sp_filter) < 3:  # handle short queries
-        return out.filter(
-            Q(species_txt__icontains=sp_filter)
-            | Q(species_name_fk__name__icontains=sp_filter)
-        )  # refactored to minimize the amount of indented code
-
-    # Test by type
-    """Note how there needs to be at least 3 characters in the query
-    to reach here.  This is to prevent 'ch' from matching 'Psychic'."""
-    try:
-        ts = Type.objects.get(name__icontains=sp_filter)
-        return out.filter(Q(species_name_fk__type1=ts) | Q(species_name_fk__type2=ts))
-    except Type.DoesNotExist:
-        pass
-    except Type.MultipleObjectsReturned:
-        pass
-
-    # Test by region/generation
-    try:
-        gen = Generation.objects.get(region__icontains=sp_filter)
-        return out.filter(species_name_fk__generation=gen)
-    except Generation.DoesNotExist:
-        pass
-    except Generation.MultipleObjectsReturned:
-        pass
-
-    # Gather previous generations
-    """This is after the short query filter to keep results at a 
-    reasonable length."""
-    q = Pokemon.objects.filter(name__icontains=sp_filter)
-    q2 = q
-    for species in q:
-        if species.evolved_from:
-            p1 = Pokemon.objects.filter(dex_number=species.evolved_from)
-            q2 = q2 | p1
-            for species1 in p1:
-                if species1.evolved_from:
-                    q2 = q2 | Pokemon.objects.filter(dex_number=species1.evolved_from)
-    q = q2.exclude(dex_number__in=[0, -999]).distinct()
-
-    # Search by species name
-    return out.filter(
-        Q(species_txt__icontains=sp_filter)
-        | Q(species_name_fk__name__icontains=sp_filter)
-        | Q(species_name_fk__in=q)
-    )
-
-
-def filter_nsla_by_species_number(out, sp_filter):
-    """
-
-    :param out:
-    :param sp_filter:
-    :return:
-    """
-    try:
-        # matching for previous evolutions so you can search for the number of the evolved form
-        q = Pokemon.objects.get(dex_number=sp_filter, form="Normal")
-        p1 = Pokemon.objects.get(dex_number=q.evolved_from, form="Normal")
-        p0 = Pokemon.objects.get(dex_number=p1.evolved_from, form="Normal")
-
-        # don't match so far back as to hit nests of commons
-        if p1.dex_number == 0:
-            p1 = Pokemon.objects.get(dex_number=-999)
-        if p0.dex_number == 0:
-            p1 = Pokemon.objects.get(dex_number=-999)
-    except Pokemon.DoesNotExist:
-        return out.filter(species_name_fk__dex_number=sp_filter)
-    return out.filter(
-        Q(species_name_fk__dex_number=sp_filter)
-        | Q(species_name_fk=p1)
-        | Q(species_name_fk=p0)
-    )
-
-
-def filter_nsla(out, sp_filter):
-    """
-
-    :param out:
-    :param sp_filter:
-    :return:
-    """
-    sp_filter = str(sp_filter).strip().lower()
-    if sp_filter == "abra":
-        # hardcode Abra so Crabwaler doesn't match®
-        return out.filter(
-            Q(species_name_fk="Abra") | Q(species_txt__lower="abra") | Q(species_no=63)
-        )
-    if "start" in sp_filter:
-        return out.filter(
-            species_name_fk__category=50
-        )  # Starter Pokémon are category 50
-    if str_int(sp_filter):  # numeric queries search by Pokédex number
-        return filter_nsla_by_species_number(out, sp_filter)
-    return filter_nsla_by_species_name(out, sp_filter)
-
-
 class CityView(generic.ListView):
     model = NstSpeciesListArchive
     template_name = "nestlist/city.html"
@@ -155,13 +46,19 @@ class CityView(generic.ListView):
     def get_queryset(self):
         srg = self.request.GET
         sp_filter = srg.get("pokemon", srg.get("species", srg.get("pokémon", None)))
-        out = NstSpeciesListArchive.objects.filter(
-            nestid__neighborhood__major_city=self.kwargs["city_id"],
-            rotation_num=get_rotation(self.kwargs.get("date", "t")),
-        ).order_by("nestid__official_name")
+        out = get_local_nsla_for_rotation(
+            get_rotation(self.kwargs.get("date", "t")), self.kwargs["city_id"], "city"
+        )
         if sp_filter is None:
             return out  # no filter
-        return filter_nsla(out, sp_filter)
+        return out.filter(
+            Q(
+                species_name_fk__in=match_species_by_name_or_number(
+                    sp_txt=sp_filter, previous_evolution_search=True
+                )
+            )
+            | Q(species_txt__icontains=sp_filter)  # for free-text row-matching
+        )
 
     def get(self, request, *args, **kwargs):
         try:
