13c340a9e70afc5ad4ffc50dc6d78f52e1012320
Chris J.M <chris@euchre.us>
2019-10-23 23:33:44 -0400

    update history view CSS

--- pokedb/speciesinfo/views.py
+++ pokedb/speciesinfo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def nothing(request, **kwargs):
+    return HttpResponse("I'll implement this later.", status=501)
