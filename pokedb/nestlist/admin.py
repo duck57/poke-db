bf348b83f9e7abcf6f2bf61255a857ccfee1a4c9
Chris J.M <chris@euchre.us>
2019-10-23 23:27:34 -0400

    memcached status page

--- pokedb/nestlist/admin.py
+++ pokedb/nestlist/admin.py
@@ -9,3 +9,5 @@ admin.site.register(NstNeighborhood)
 admin.site.register(NstCombinedRegion)
 admin.site.register(NstAltName)
 admin.site.register(NstParkSystem)
+# memcache status page here b/c this is the heaviest use
+admin.site.index_template = "memcache_status/admin_index.html"
