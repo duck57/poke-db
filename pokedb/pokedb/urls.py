c53ee0b3d69ddc0092c1d56aa3e86ddaba9cc28e
Chris J.M <chris@euchre.us>
2019-12-11 22:01:39 -0500

    Remove unused debug tools

--- pokedb/pokedb/urls.py
+++ pokedb/pokedb/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
 
 urlpatterns = [
     path("admin2/", admin.site.urls),
@@ -8,12 +7,3 @@ urlpatterns = [
     # path('cp/', include('pokeperfect.urls', namespace='perfect')),
     path("city/", include("nestlist.urls", namespace="nestlist")),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
