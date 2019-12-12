8883eb2015ec8a4c0eb2be8ef8603d1b27c31631
Chris J.M <chris@euchre.us>
2019-12-11 22:13:28 -0500

    slight tweaks to URL schemes

--- pokedb/pokedb/urls.py
+++ pokedb/pokedb/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin2/", admin.site.urls),
     path("pokedex/", include("speciesinfo.urls", namespace="pokedex")),
-    # path('cp/', include('pokeperfect.urls', namespace='perfect')),
+    # path("cp/", include("pokeperfect.urls", namespace="perfect")),
     path("city/", include("nestlist.urls", namespace="nestlist")),
 ]
